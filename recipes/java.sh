442da6a0910c978f276ee78e5aebe48359989a16
David Rio Deiros <driodeiros@gmail.com>
2010-10-27 14:09:42 -0500

    * Fixing java construct.sh

--- recipes/java.sh
+++ recipes/java.sh
@@ -1,7 +1,7 @@
 
 # This recipe is special, please read: https://jdk-distros.dev.java.net/developer.html
 local URL="http://download.java.net/dlj/binaries/jdk-6u22-dlj-linux-amd64.bin"
-local construct="https://jdk-distros.dev.java.net/source/browse/*checkout*/jdk-distros/trunk/utils/construct.sh?content-type=text%2Fplain"
+local construct='https://jdk-distros.dev.java.net/source/browse/*checkout*/jdk-distros/trunk/utils/construct.sh?content-type=text%2Fplain'
 local tb_file=`basename $URL`
 local seed_name=$recipe
 local install_files=(bin/java bin/javac)
@@ -19,7 +19,7 @@ do_install()
   sh $tb_file --accept-license &> $LOG_DIR/$recipe.unpack.txt
   # pwd: tarball
   cd ..
-  curl -sLO $construct
+  curl -sL $construct > construct.sh
   sh ./construct.sh $recipe linux-jdk
   mv linux-jdk ../local/java
   link_from_stage $recipe ${install_files[@]}
