a0288868602be38905f5da432e067a7d84040e65
David Rio Deiros <driodeiros@gmail.com>
2010-10-27 16:46:19 -0500

    * Adding apr.

---
+++ recipes/apr.sh
@@ -0,0 +1,29 @@
+
+local URL="http://apache.securedservers.com/apr/apr-1.4.2.tar.gz"
+local tb_file=`basename $URL`
+local type="tar.gz"
+local seed_name=$(extract_tool_name $tb_file $type)
+
+do_install()
+{
+  before_install $seed_name
+  cd $TB_DIR
+  download $URL $tb_file
+  decompress_tool $tb_file $type
+  cd $seed_name
+  configure_tool $seed_name $LOCAL_DIR
+  make_tool $seed_name
+  install_tool $seed_name
+  #link_from_stage $seed_name ${install_files[@]}
+  after_install $seed_name
+}
+
+do_remove()
+{
+  before_remove $seed_name
+  remove_recipe_using_make $seed_name
+  #remove_from_stage $seed_name ${install_files[@]}
+  after_remove $seed_name
+}
+
+source "$MAIN_DIR/lib/case.sh"
