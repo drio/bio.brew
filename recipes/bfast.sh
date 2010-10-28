1a81f7eb44790766557b9cb3d7b09e362bc0962c
David Rio Deiros <driodeiros@gmail.com>
2010-10-28 10:04:59 -0500

    * adding bfast.

---
+++ recipes/bfast.sh
@@ -0,0 +1,30 @@
+
+local URL="git://bfast.git.sourceforge.net/gitroot/bfast/bfast"
+local tb_file=`basename $URL`
+local seed_name="bfast"
+local install_files=(bfast/bfast scripts/solid2fastq scripts/ill2fastq.pl)
+
+do_install()
+{
+  before_install $seed_name
+  cd $LOCAL_DIR
+  log "git cloning: $URL"
+  git clone $URL &> $LOG_DIR/${seed_name}.git_clone.log.txt
+  cd $seed_name
+  log "autogen"
+  sh ./autogen.sh &> $LOG_DIR/${seed_name}.autogen.log.txt
+  configure_tool $seed_name 
+  make_tool $seed_name $make_j
+  link_from_stage $recipe ${install_files[@]}
+  after_install $recipe
+}
+
+do_remove()
+{
+  before_remove $seed_name
+  remove_recipe $seed_name
+  remove_from_stage $seed_name ${install_files[@]}
+  after_remove $seed_name
+}
+
+source "$MAIN_DIR/lib/case.sh"
