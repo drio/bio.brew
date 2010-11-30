ead0f4b8156f553df56dc39b39bb60cf85cb7437
David Rio Deiros (drio) <deiros@bcm.edu>
2010-11-30 15:14:07 -0600

    * adding pv

---
+++ recipes/pv.sh
@@ -0,0 +1,29 @@
+local URL="http://pipeviewer.googlecode.com/files/pv-1.1.4.tar.bz2"
+local tb_file=`basename $URL`
+local type="tar.bz2"
+local seed_name=$(extract_tool_name $tb_file $type)
+local install_files=(bin/pv)
+
+do_install()
+{
+  before_install $seed_name
+  cd $TB_DIR
+  download $URL $tb_file
+  decompress_tool $tb_file $type
+  cd $seed_name
+  configure_tool $seed_name
+  make_tool $seed_name $make_j
+  install_tool $seed_name
+  link_from_stage $seed_name ${install_files[@]}
+  after_install $seed_name
+}
+
+do_remove()
+{
+  before_remove $seed_name
+  remove_recipe $seed_name
+  remove_from_stage $seed_name ${install_files[@]}
+  after_remove $seed_name
+}
+
+source "$MAIN_DIR/lib/case.sh"
