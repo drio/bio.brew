8bf7f355d3c0faf1965e7813c6710a48fb9ce02f
David Rio Deiros (drio) <deiros@bcm.edu>
2011-04-20 12:04:57 -0500

    * Changes in java jdk tarball.

--- recipes/java.sh
+++ recipes/java.sh
@@ -1,10 +1,11 @@
 
 # This recipe is special, please read: https://jdk-distros.dev.java.net/developer.html
-local URL="http://download.java.net/dlj/binaries/jdk-6u22-dlj-linux-amd64.bin"
+local URL="http://download.java.net/dlj/binaries/jdk-6u24-dlj-linux-amd64.bin"
+#local URL="http://download.java.net/dlj/binaries/jdk-6u24-dlj-linux-i586.bin"
 local construct='https://jdk-distros.dev.java.net/source/browse/*checkout*/jdk-distros/trunk/utils/construct.sh?content-type=text%2Fplain'
 local tb_file=`basename $URL`
 local seed_name="java"
-local install_files=(bin/java bin/javac)
+local install_files=(bin/java bin/javac bin/jar)
 
 do_install()
 {
@@ -16,14 +17,12 @@ do_install()
   mv $tb_file $recipe
   # pwd: tarball/java
   cd $recipe
+  log "Untaring jdk"
   sh $tb_file --accept-license &> $LOG_DIR/$recipe.unpack.txt
-  # pwd: tarball
   cd ..
-  log "downloading construst.sh"
-  curl -sL $construct > construct.sh
-  sh ./construct.sh $recipe linux-jdk linux-jre &>/dev/null
-  rm -rf linux-jre
-  mv linux-jdk ../local/java
+  # pwd: tarball
+  log "Moving to local"
+  mv java/jdk1* ../local/java
   link_from_stage $recipe ${install_files[@]}
   after_install $recipe
 }
