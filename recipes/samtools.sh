2a31cde4869f4f720143c45d448ed2ff972c28f0
David Rio Deiros <driodeiros@gmail.com>
2011-12-05 17:24:14 -0600

    * Leaving in the edge: pulling samtools from github

--- recipes/samtools.sh
+++ recipes/samtools.sh
@@ -1,23 +1,20 @@
-local version="0.1.16"
-local URL="http://downloads.sourceforge.net/project/samtools/samtools/${version}/samtools-${version}.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsamtools%2Ffiles%2Fsamtools%2F${version}%2F&ts=1320169674&use_mirror=voxel"
-local tb_file=`basename $URL`
-local type="tar.bz2"
-local seed_name="samtools-${version}"
+local URL="http://github.com/lh3/samtools.git"
+local seed_name="samtools"
+local deps=()
 local install_files=(samtools misc/samtools.pl bcftools/bcftools bcftools/vcfutils.pl)
 
 do_install()
 {
+  check_deps ${deps[@]}
   before_install $seed_name
-  cd $TB_DIR
-  download $URL $tb_file
-  decompress_tool $tb_file $type
+  cd $LOCAL_DIR
+  log "git cloning: $URL"
+  git clone $URL &> $LOG_DIR/${seed_name}.git_clone.log.txt
   cd $seed_name
+  log "make"
   make_tool $seed_name $make_j
-  cd ..
-  mv $seed_name $LOCAL_DIR
-  link_from_stage $seed_name ${install_files[@]}
-  for_env "export SAMTOOLS_DIR='$LOCAL_DIR/$seed_name'"
-  after_install $seed_name
+  link_from_stage $recipe ${install_files[@]}
+  after_install $recipe
 }
 
 do_remove()
